f183aecc1b4b0ed862c6274a9a7a303288d7dd4d
Ziyodullo <firdavssharopov946@gmail.com>
2026-04-30 15:42:05 +0500

    adminlarni qoshdim

--- src/apps/todoList/admin.py
+++ src/apps/todoList/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin  # noqa
+from apps.todoList.models import Todo
 
 # Register your models here.
+
+@admin.register(Todo)
+class TodoModelAdmin(admin.ModelAdmin):
+    list_display=['title', 'dedline']
+
